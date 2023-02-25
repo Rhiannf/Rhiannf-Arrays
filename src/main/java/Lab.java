dc55b4b0e746ca3acf0b1bd0faac85ea5836f557
RhiannF <rhiannf04@gmail.com>
2023-02-25 16:44:37 +0000

    Arrays

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -47,6 +47,6 @@ public class Lab {
      * @return a new int array that can contain n elements.
      */
     public int[] returnNewArraySizeN(int n){
-        return null;
+        return new int[n] ;
     }
 }
